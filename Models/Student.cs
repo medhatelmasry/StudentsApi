cc8a6091fa98dab249fe0b48649d4bee047d18d7
Medhat Elmasry <medhat_elmasry@bcit.ca>
2023-02-02 20:24:01 -0800

    format C# code

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,16 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+namespace StudentsApi.Models;
 
-namespace StudentsApi.Models
+public class Student
 {
-    public class Student
-    {
-        public int StudentId { get; set; }
-        public string? LastName { get; set; }
-        public string? FirstName { get; set; }
-        public string? School { get; set; }
-    }
-
+    public int StudentId { get; set; }
+    public string? LastName { get; set; }
+    public string? FirstName { get; set; }
+    public string? School { get; set; }
 }
